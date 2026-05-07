9dc96268fffe36c318d7aee25726e62cafa62a31
SOUMILCHANDRA <SOUMILCHANDRA@GMAIL.COM>
2026-05-07 21:58:15 +0530

    data(registry): update circuit metadata and exports

--- src/assets/data/CircuitRegistry.ts
+++ src/assets/data/CircuitRegistry.ts
@@ -1,10 +1,10 @@
-import monza from '../assets/data/circuits/monza.json'
-import silverstone from '../assets/data/circuits/silverstone.json'
-import spa from '../assets/data/circuits/spa.json'
-import monaco from '../assets/data/circuits/monaco.json'
-import suzuka from '../assets/data/circuits/suzuka.json'
-import interlagos from '../assets/data/circuits/interlagos.json'
-import austin from '../assets/data/circuits/austin.json'
+import monza from './circuits/monza.json'
+import silverstone from './circuits/silverstone.json'
+import spa from './circuits/spa.json'
+import monaco from './circuits/monaco.json'
+import suzuka from './circuits/suzuka.json'
+import interlagos from './circuits/interlagos.json'
+import austin from './circuits/austin.json'
 
 export const circuits = {
   Monza: monza,
